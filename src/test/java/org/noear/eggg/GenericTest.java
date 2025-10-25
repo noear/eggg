88d9db6bc28dc535f7b05ea2dd2ce50d21105db1
noear <noear@live.cn>
2025-10-25 11:40:31 +0800

    优化代码

--- src/test/java/org/noear/eggg/GenericTest.java
+++ src/test/java/org/noear/eggg/GenericTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 /**
  *
@@ -18,5 +19,36 @@ public class GenericTest {
         Map<String, Object> map = new HashMap<>();
 
         eggg.getTypeEggg(map.getClass()).getClassEggg();
+    }
+
+    @Test
+    public void case2() {
+        ClassEggg classEggg = eggg.getTypeEggg(C.class).getClassEggg();
+
+        assert classEggg.getFieldEgggByName("x").getType() == List.class;
+        assert classEggg.getFieldEgggByName("x").getTypeEggg().isParameterizedType();
+        assert classEggg.getFieldEgggByName("x").getTypeEggg().getActualTypeArguments()[0] == String.class;
+
+        assert classEggg.getFieldEgggByName("y").getType() == Map.class;
+        assert classEggg.getFieldEgggByName("y").getTypeEggg().isParameterizedType();
+        assert classEggg.getFieldEgggByName("y").getTypeEggg().getActualTypeArguments()[0] == String.class;
+        assert classEggg.getFieldEgggByName("y").getTypeEggg().getActualTypeArguments()[1] == Integer.class;
+
+        assert classEggg.getFieldEgggByName("m").getType() == String.class;
+        assert classEggg.getFieldEgggByName("n").getType() == Integer.class;
+    }
+
+    public static class A<X, Y> {
+        public X x;
+        public Y y;
+    }
+
+    public static class B<M, N> extends A<List<M>, Map<String, N>> {
+        public M m;
+        public N n;
+    }
+
+    public static class C extends B<String, Integer> {
+
     }
 }
